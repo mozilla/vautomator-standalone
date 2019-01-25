4b1b14f2fde23a159b7a9aebfb3290e0833d30fe
Jonathan Claudius <jclaudius@mozilla.com>
2019-01-25 13:32:40 -0500

    Show how to upgrade classes without touching task right away:

---
+++ tests/test_tlsobs_scan.py
@@ -0,0 +1,9 @@
+from distutils.spawn import find_executable
+import logging
+import coloredlogs
+import subprocess
+from lib import utils
+from lib.task import Task
+from lib.tlsobs_scan import MozillaTLSObservatoryTask
+
+# ADD THE TESTS FOR TLSOBS SCAN
